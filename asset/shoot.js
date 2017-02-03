ae1491878270805474abdf874efe5bb0c3c15302
Loan <campan.loan78@gmail.com>
2017-02-03 14:32:49 +0100

    ajout des météores et des collisions

--- asset/shoot.js
+++ asset/shoot.js
@@ -1,21 +1,21 @@
-var	nb_click=0
 $("canvas").mousedown(function()
 	{
-		if (shoot_tab.length<3) 
+		if (shoot_tab.length<1) 
 		{
 			shoot_tab.push(shoot(spaceship.position))
 		}
 	})
-
 function shoot(pos)
 {
+	var shoot_sound= new Audio('asset/sound/shoot.ogg');
+	shoot_sound.play();
 	var geometry = new THREE.CylinderGeometry( .5, .5, 6, 16 );
 	var material = new THREE.MeshBasicMaterial( {color: 0xff0000} );
 	var cylinder = new THREE.Mesh( geometry, material );
 	scene.add( cylinder );
-	cylinder.rotation.z = Math.PI/2;
 	cylinder.position.x = pos.x+10
 	cylinder.position.y = pos.y
 	cylinder.position.z = pos.z
+	cylinder.rotation.z = Math.PI/2;
 	return cylinder;
 }
